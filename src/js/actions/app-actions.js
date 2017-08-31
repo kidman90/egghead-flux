1dd20cfb95952f2f8f8e46d61757447913404901
kidman90 <kacprzak90@gmail.com>
2017-08-31 23:08:11 +0200

    04 Flux Architecture: Application Store

--- src/js/actions/app-actions.js
+++ src/js/actions/app-actions.js
@@ -1,5 +1,5 @@
 import AppConstants from '../constants/app-constants';
-import {dispatch, register} from '../dispatchers/app-dispatcher';
+import { dispatch, register } from '../dispatchers/app-dispatcher';
 
 export default {
   addItem(item) {
